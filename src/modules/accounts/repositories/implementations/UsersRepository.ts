d49a4c742206777ad6478b03e6dc7d46e1b78540
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-11-07 22:18:54 -0300

    feat: add users route

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,23 @@
+import { getRepository, Repository } from "typeorm";
+
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({ name, username, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({ name, username, email, driver_license, password });
+
+    await this.repository.save(user);
+  }
+
+}
+
+export { UsersRepository }
