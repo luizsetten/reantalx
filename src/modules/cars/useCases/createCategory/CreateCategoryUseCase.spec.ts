0c41d2d640b2343fd6a00f9dc8612b5c01b64873
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-11-12 16:11:32 -0300

    feat: created tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,11 @@
+
+
+describe("Create category", () => {
+  it("2 + 2 should be 4", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it("2 + 2 shouldn't be 5", () => {
+    expect(2 + 2).not.toBe(5);
+  })
+});
